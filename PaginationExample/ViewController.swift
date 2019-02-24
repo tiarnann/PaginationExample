5ba533de177441546b20cef9433f1297bf19ee2c
Tiarnan McGrath <mcgratti@tcd.ie>
2019-02-24 21:33:02 +0000

    Initial Commit

---
+++ PaginationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaginationExample
+//
+//  Created by Tíarnán McGrath on 24/02/2019.
+//  Copyright © 2019 Tíarnán McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

a4d5c7230d05ae47a4f4b031cff7459226b9c6b6
Tiarnan McGrath <mcgratti@tcd.ie>
2019-02-24 23:17:01 +0000

    Making new cell with some extensions on UITableView, UITableViewCell

---
+++ PaginationExample/Posts/ViewController/Cells/PostsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostsTableViewCell.swift
+//  PaginationExample
+//
+//  Created by Tíarnán McGrath on 24/02/2019.
+//  Copyright © 2019 Tíarnán McGrath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+}
---
+++ PaginationExample/Utils/UITableView+Registering.swift
@@ -0,0 +1,26 @@
+//
+//  UITableView+Registering.swift
+//  PaginationExample
+//
+//  Created by Tíarnán McGrath on 24/02/2019.
+//  Copyright © 2019 Tíarnán McGrath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView {
+    func register(_ cellClass: UITableViewCell.Type) {
+        self.register(cellClass, forCellReuseIdentifier: cellClass.identifier)
+    }
+    
+    func dequeueResusableCell<CellType: UITableViewCell>(for indexPath: IndexPath) -> CellType {
+        return self.dequeueReusableCell(withIdentifier: CellType.identifier, for: indexPath) as! CellType
+    }
+}
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: type(of: self))
+    }
+}
